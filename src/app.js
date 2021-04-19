f138c16bab0fd9ade099d219c3f69c96de2bacbb
donaldcrane <donaldagbakoba@gmail.com>
2021-04-20 00:24:06 +0100

    modified port

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import router from "./routes/index";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
